d6e85f8615d42a0d594c4f49768bb862464ffb97
Merdan Özdogan <merdanoe@mbp-von-merdan.localdomain>
2024-02-05 13:01:30 +0100

    second alternative

---
+++ main.js
@@ -0,0 +1,8 @@
+import "./styles/style.scss";
+import "./styles/hero.scss";
+import "./styles/skills.scss";
+import "./styles/projects.scss";
+import "./styles/contact.scss";
+import "./styles/footer.scss";
+import "./styles/fontface.scss";
+import "./styles/header.scss";
